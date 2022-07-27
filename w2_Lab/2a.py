d9f4ca8a17fa6e86d9725f6e23a3e5aa7a6b7696
Cordon Jr <ecordonjr@email.neit.edu>
2022-07-27 08:19:12 -0400

    Finished LAB 2A (Group Assingmnet)

--- w2_Lab/2a.py
+++ w2_Lab/2a.py
@@ -1,4 +1,4 @@
-#Lab 2A
+#Lab 2A Class Lav
 
 #Prompt: The csv file lab2a.csv contains a list of rooms, the maximum number of people that the room can accommodate, and the number of people currently registered for the event.  Write a program that displays all rooms that are over the maximum limit of people and the number of people that have to be notified that they will have to be put on the wait list. After the file is completely processed the program should display the number of records processed and the number of rooms that are over the limit.
 
@@ -10,7 +10,7 @@ total_rooms = 0 # total rooms
 
 print("{0:19}\t{1:5}\t{2:4}\t{3:4}".format("Room", "Max", "Min", "Over"))
 
-with open("C:/Users/Erick/Desktop/SE126_202240/SE126_202240/w2_Lab/lab2a.csv") as csvfile:
+with open("w2_Lab\lab2a.csv") as csvfile:
 
     file = csv.reader(csvfile)
 
@@ -22,15 +22,14 @@ with open("C:/Users/Erick/Desktop/SE126_202240/SE126_202240/w2_Lab/lab2a.csv") a
         room = rec[0]
         max = int(rec[1])
         min = int(rec[2])
-        over = min - max
+        over = (max - min) *-1
+        
+        if over >= 0:
+        
 
-        if min > max:
-            #print(over)
-            total_rooms += over
+            total_rooms += 1
 
-    #total_rooms += over
-
-    print("{0:18}\t{1:4}\t{2:4}\t{3:4}".format(room, max, min, over))
+            print("{0:18}\t{1:3}\t{2:3}\t{3:3}".format(room, max, min, over))
     
 print("\nTotal records processed are {}".format(total_records))
 print("\nTotal rooms over the limit are {}".format(total_rooms))
