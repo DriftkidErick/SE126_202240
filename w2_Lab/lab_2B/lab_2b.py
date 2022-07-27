3a47dc0e80a323b7d8a69ac72ac08b290a7a4785
Cordon Jr <ecordonjr@email.neit.edu>
2022-07-27 08:39:18 -0400

    Start

---
+++ w2_Lab/lab_2B/lab_2b.py
@@ -0,0 +1,8 @@
+#lab 2b Erick Cordon
+
+#Prompt: You have been asked to produce a report that lists all the computers in the csv file lab2b.csv. Your report should look like the following sample output. The last line should print the number of computers in the file.
+
+#main code below----
+
+import csv
+
