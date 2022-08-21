f4eb181b30b76aad8baddd5566cc3558d3ac7478
DriftkidErick <94418693+DriftkidErick@users.noreply.github.com>
2022-08-20 20:02:25 -0400

    Finished Most just wrap up work

--- Midterm/midterm.py
+++ Midterm/midterm.py
@@ -23,12 +23,26 @@ def menu():
 
     return choice
 
-def picker(value):
+def mood():
 
-    for i in range(0,1): 
-        song = rec[0]
-        value = random.choices(song)
-        print("\nToday we recommend you listen to this album: {}\n".format(value))
+    print("1. The College Dropout ")
+    print("2. Late Registration")
+    print("3. Graduation")
+    print("4. 808s & Heartbreak")
+    print("5. My Beautiful Dark Twisted Fantasy")
+    print("6. Yeezus")
+    print("7. The Life Of Pablo")
+    print("8. Ye")
+    print("9. Jesus Is King")
+    print("10. Donda")
+    
+    choice2 = int(input("Please Select One of the following: "))
+
+    while choice2 < 0 or choice2 > 10:
+        print("*Error!*")
+        choice2 = int(input("Please enter your selection: "))
+
+    return choice2
 
 
 
@@ -37,12 +51,11 @@ rating = []
 sales = []
 year = []
 cost = []
-value = 0
 num_avg = []
 
 records = 0
 
-with open ("/Users/erickcordon/Desktop/SE126_202240/Midterm/KanyeSales.csv") as csvfile:
+with open ("C:/Users/Erick/Desktop/SE126_202240/Midterm/KanyeSales.csv") as csvfile:
 
     file = csv.reader(csvfile)
 
@@ -76,17 +89,17 @@ while pick != 5:
         for i in range(0,records):
             print("{0:32}\t{1:18}\t{2:8}\t".format(album[i],year[i],sales[i]))
 
-        print("---------------------------------------------------------\n")
+        print ("---------------------------------------------------------------------------------------------")
 
     
     if pick == 2:
 
         print("-------------------------------------------------------------------------------------------------------------")
-        print("Album                                   Date                     Sales \t\t\tGross Sales")
+        print("Album                                   Date                     Sales \t\tGross Sales")
         print("-------------------------------------------------------------------------------------------------------------")
 
         for i in range(0,records):
-            print("{0:32}\t{1:18}\t{2:8}\t\t{3:0.2f}".format(album[i],year[i],sales[i],num_avg[i]))
+            print("{0:32}\t{1:18}\t{2:8}\t{3:0.2f}".format(album[i],year[i],sales[i],num_avg[i]))
         print("-------------------------------------------------------------------------------------------------------------")
 
 
@@ -94,7 +107,7 @@ while pick != 5:
     if pick == 3:
 
         print("-------------------------------------------------------------------------------------------------------------")
-        print("Album                                   Date                     Sales \t\tScore\t\tStar")
+        print("Album                                   Date                     Sales \t\tScore   Star")
         print("-------------------------------------------------------------------------------------------------------------")
 
         for i in range(0,records):
@@ -106,12 +119,132 @@ while pick != 5:
             
             star.append(star_add)
 
-            print("{0:32}\t{1:18}\t{2:8}\t {3}\t\t{4:5}".format(album[i],year[i],sales[i],rating[i],star[i]))
+            print("{0:32}\t{1:18}\t{2:8}\t {3}\t{4:5}".format(album[i],year[i],sales[i],rating[i],star[i]))
 
     if pick == 4:
 
-        song = picker(value)
+        gen = mood()
+
+        while gen != 11:
+
+            if gen == 1:
+
+                tcd = ["Intro","We Dont Care", "Graduation Day","All Falls Down","I'll Fly Away","Spaceship","Jesus Walks","Never Let Me Down","Get Em High","Workout Plan","The New Workout Plan","Slow Jamz","Breathe In Breathe Out","School Spirit Skit","School Spirit Skit 2","Lil Jimmy SKit","Two Words","Through The Wire","Family Business"]
+
+                song = random.choice(tcd)
+                print("\n")
+                print("The song we recommend you listen to today is: {}".format(song))
+                print("\n")
+                break
+                
+            if gen == 2:
+
+                lr = ["Wake Up Mr.West","Heard 'Em Say","Touch The Sky","Gold Digger","Drive Slow","My Way Homme","Crack Music","Roses","Bring Me Down","Addiction","Diamonds From Sierra Leone","We Major","Hey Mama","Celebration","Gone","Diamonds from Sierra Leone [remix]","Late"]
+
+                song = random.choice(lr)
+                print("\n")
+                print("The song we recommend you listen to today is: {}".format(song))
+                print("\n")
+                break
+
+            if gen == 3:
+
+                g = ["Good Morning","Champion","Stronger","I Wonder","Good Life","Can't Tell Me Nothing","Barry Bonds","Drunk and Hot","Flashing Lights","Everything","The GLory","Homecoming","Big Brother"]
+
+                song = random.choice(g)
+                print("\n")
+                print("The song we recommend you listen to today is: {}".format(song))
+                print("\n")
+                break
+
+            if gen == 4:
+
+                hrtbrk = ["Say You Will","Welcome To Heartbreak","Heartless","Amazing","Love Lockdown","Paranoid","RoboCop","Street Lights","Bad News","See You In My Nightmares","Coldest Winter","Pinocchio Story"]
+
+                song = random.choice(hrtbrk)
+                print("\n")
+                print("The song we recommend you listen to today is: {}".format(song))
+                print("\n")
+                break
+
+            if gen == 5:
+
+                mbdtf = ["Dark Fantasy","Gorgeous","Power","All Of The Lights (Interlude)","All of the Lights","Monster","So Appalled","Devil In A New Dress","Runaway","Hell Of A Life","Blame Game","Lost In The World","Who Will Survive In America"]
+
+                song = random.choice(mbdtf)
+                print("\n")
+                print("The song we recommend you listen to today is: {}".format(song))
+                print("\n")
+                break
+
+            if gen == 6:
+
+                zus = ["On Sight","Skinhead","I am A God","New","Hold My Liquor","I'm In it","Blood On The Leaves","Guilt Trip","Send It Up","Bound 2"]
+
+                song = random.choice(zus)
+                print("\n")
+                print("The song we recommend you listen to today is: {}".format(song))
+                print("\n")
+                break
+
+            if gen == 7:
+
+                tlp = ["Ultralight Beam","Father Stretch My Hands","Pt.2","Famous","Feedback","Low Lights","Highlights","Freestyle 4","I Love Kanye","Waves","FML","Real Friends","Wolves","Franks track","30 Hours","No More Parties In LA","Facts","Fade","Saint Pablo"]
+
+                song = random.choice(tlp)
+                print("\n")
+                print("The song we recommend you listen to today is: {}".format(song))
+                print("\n")
+                break
+
+            if gen == 8:
+
+                ye = ["I Thought About you","Yikes","All Mines","Wouldn't Leave","No Mistakes","Ghost Town","Violent Crimes"]
+
+                song = random.choice(ye)
+                print("\n")
+                print("The song we recommend you listen to today is: {}".format(song))
+                print("\n")
+                break
+
+            if gen == 9:
+
+                jik = ["Every Hour","Selah","Follow God","Closed On Sunday","On God","Everything We Need","Water","God Is","Hands On","Use This Gospel","Jesus is Lord"]
+
+                song = random.choice(jik)
+                print("\n")
+                print("The song we recommend you listen to today is: {}".format(song))
+                print("\n")
+                break
+
+            if gen == 10:
+
+                don = ["Donda Chant","Hurricane","Moon","Life Of The Party","Off The Grid","Jail","Praise God","Come To Life","Believe What I Say","No Child Left Behind","Up From The Ashes","Remote Control Pt.2","God Breathed","Lord I Need You","24","Junya","Never Abandon Your Family","Donda","Keep My Spirit Alive","Jesus Is Lord Pt.2","Heaven And Hell","Remote Control","Tell The Vision","Jonah","Pure Souls","Ok Ok","New Again","Junya Pt. 2","Jail Pt.2","Keepy My Spirit Alive Pt.2"]
+
+                song = random.choice(don)
+                print("\n")
+                print("The song we recommend you listen to today is: {}".format(song))
+                print("\n")
+                break
+
+
+
+
+
+
+
+
+
             
+
+
+
+
+
+
+
+
+
 
     pick = menu()
 
