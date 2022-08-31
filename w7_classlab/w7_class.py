0549cc093d74e43f2e6b835cb37db8e96ebcf2f1
DriftkidErick <94418693+DriftkidErick@users.noreply.github.com>
2022-08-31 15:34:05 -0400

    Testing list replacement

--- w7_classlab/w7_class.py
+++ w7_classlab/w7_class.py
@@ -30,14 +30,126 @@ for i in range(0,7):
     print(i + 1, seatA[i],seatB[i],seatC[i],seatD[i])
 
 
-r = int(input("\nPlease select which ROW you would like to be seated in [1-7]: "))
+row = int(input("Please enter the row you would like to sit in [1-7]: "))
 
-while r == 1:
-    seatA = input("Please select which SEAT you would like to sit in [A-D]: ").upper()
-    if seatA[r-1] == "A":
-        seatA[r-1].remove("A","X")
-        print(seatA,seatB)
+r = (row -1)
 
-    else:
-        print("**erorr**")
+while r  >=0 or r <= 7 :
+
+    s = input("Please enter the seat you would like [A - D]: ").upper()
+    if r == 0:    
+        if s == "A":
+            seatA[0] = "x"
+            print(seatA)
+
+        elif s == "B":
+            seatB[0] = "x"
+            print(seatA)
+        
+        elif s == "C":
+            seatC[0] = "x"
+            print(seatA)
+
+        elif s == "D":
+            seatD[0] = "x"
+
+        for i in range(0,7):
+            print(i + 1, seatA[i],seatB[i],seatC[i],seatD[i])
+        
+    if r == 1:    
+        if s == "A":
+            seatA[1] = "x"
+            print(seatA)
+
+        elif s == "B":
+            seatB[1] = "x"
+            print(seatA)
+        
+        elif s == "C":
+            seatC[1] = "x"
+            print(seatA)
+
+        elif s == "D":
+            seatD[1] = "x"
+
+        for i in range(0,7):
+            print(i + 1, seatA[i],seatB[i],seatC[i],seatD[i])
+    
+    if r == 2:    
+        if s == "A":
+            seatA[2] = "x"
+            print(seatA)
+
+        elif s == "B":
+            seatB[2] = "x"
+            print(seatA)
+        
+        elif s == "C":
+            seatC[2] = "x"
+            print(seatA)
+
+        elif s == "D":
+            seatD[2] = "x"
+
+        for i in range(0,7):
+            print(i + 1, seatA[i],seatB[i],seatC[i],seatD[i])
+
+    if r == 3:    
+        if s == "A":
+            seatA[3] = "x"
+            print(seatA)
+
+        elif s == "B":
+            seatB[3] = "x"
+            print(seatA)
+        
+        elif s == "C":
+            seatC[3] = "x"
+            print(seatA)
+
+        elif s == "D":
+            seatD[3] = "x"
+
+        for i in range(0,7):
+            print(i + 1, seatA[i],seatB[i],seatC[i],seatD[i])
+
+    if r == 4:    
+        if s == "A":
+            seatA[4] = "x"
+            print(seatA)
+
+        elif s == "B":
+            seatB[4] = "x"
+            print(seatA)
+        
+        elif s == "C":
+            seatC[4] = "x"
+            print(seatA)
+
+        elif s == "D":
+            seatD[4] = "x"
+
+        for i in range(0,7):
+            print(i + 1, seatA[i],seatB[i],seatC[i],seatD[i])
+
+    if r == 5:    
+        if s == "A":
+            seatA[5] = "x"
+            print(seatA)
+
+        elif s == "B":
+            seatB[5] = "x"
+            print(seatA)
+        
+        elif s == "C":
+            seatC[5] = "x"
+            print(seatA)
+
+        elif s == "D":
+            seatD[5] = "x"
+
+        for i in range(0,7):
+            print(i + 1, seatA[i],seatB[i],seatC[i],seatD[i])
+    
+
 
