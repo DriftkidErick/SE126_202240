2d0a1fd33ba534c85897236a87f725c414d14580
DriftkidErick <94418693+DriftkidErick@users.noreply.github.com>
2022-08-31 21:56:34 -0400

    Rough draft and base copy

---
+++ w7_classlab/classtest.py
@@ -0,0 +1,65 @@
+from os import system, name 
+from time import sleep 
+def clear():  
+
+     # for windows  
+     if name == 'nt':
+          _ = system('cls') 
+
+def more():
+
+    answer = input("Would you like to add another seat? [y/n]: ").lower()
+
+    if answer == "n".lower():
+        print("have a good day")
+
+    elif answer != "y" and answer != "n":
+        answer = input("Would you like to add another seat? [y/n]: ").lower()
+
+    
+    return (answer)
+
+#Main Code Below--------------------------
+
+seatA = ['A','A','A','A','A','A','A']
+seatB = ['B','B','B','B','B','B','B']
+seatC = ['C','C','C','C','C','C','C']
+seatD = ['D','D','D','D','D','D','D']
+
+print("Row")
+
+for i in range(0,7):
+    print("{}  {} {}\t{} {}".format(i + 1, seatA[i],seatB[i],seatC[i],seatD[i]))
+
+answer = "y"
+
+while answer =="y":
+    answer = int(input("Please enter the row you would like to sit in [1-7]: "))
+
+    r = (answer -1)    
+
+    s = input("Please enter the seat you would like [A - D]: ").upper()
+
+    if r >= 0 or r <= 7 :    
+        if s == "A":
+            seatA[r] = "x"
+            
+
+        elif s == "B":
+            seatB[r] = "x"
+            
+        
+        elif s == "C":
+            seatC[r] = "x"
+            
+
+        elif s == "D":
+            seatD[r] = "x"
+
+        for i in range(0,7):
+            print(i + 1, seatA[i],seatB[i],seatC[i],seatD[i])
+
+    more() #The function that asks the user if they want to add another seat
+
+
+print("out of loop")
--- w7_classlab/w7_class.py
+++ w7_classlab/w7_class.py
@@ -2,22 +2,32 @@
 
 #Prompt: Write a Python program to assign passengers seats in an airplane.  Assume a small airplane with seat numbering as follows. The program should display the seat pattern, with an ‘X’ making the seats already assigned. For example, after seats 1A, 2B and 4C are taken the display should look like this: After displaying the seats available, the program prompts for the seat desired, the user types in a seat and then the display of available seats is updated.  This continues until all seats are filled or until the user signals that the program should end.  If a user types in a seat that is already assigned, the program should say that the seat is occupied and ask for another choice.
 
-#Varrible Def:
+#Varrible Def: 
+from os import system, name 
 
-def selector():
-    r = input("\nPlease select which ROW you would like to be seated in [1-7]: ")
+from time import sleep 
 
-    s= input("Please select which Seat you would like to sit in [A-D]: ").upper()
+def clear():  
 
-    return (r,s)
+     # for windows  
+     if name == 'nt':
+          _ = system('cls') 
 
+'''def more():
 
+    answer = input("Would you like to add another seat? [y/n]: ").lower()
 
+    if answer == "n".lower():
+        print("have a good day")
+
+    elif answer != "y" and answer != "n":
+        answer = input("Would you like to add another seat? [y/n]: ").lower()
 
     
+    return (answer)
+'''
 
 #Main Code Below-----------------------------------------------------
-
 
 seatA = ['A','A','A','A','A','A','A']
 seatB = ['B','B','B','B','B','B','B']
@@ -30,126 +40,49 @@ for i in range(0,7):
     print(i + 1, seatA[i],seatB[i],seatC[i],seatD[i])
 
 
-row = int(input("Please enter the row you would like to sit in [1-7]: "))
+answer = "y"
 
-r = (row -1)
+while answer =="y":
+    answer = int(input("Please enter the row you would like to sit in [1-7]: "))
 
-while r  >=0 or r <= 7 :
+    r = (answer -1)    
 
     s = input("Please enter the seat you would like [A - D]: ").upper()
-    if r == 0:    
-        if s == "A":
-            seatA[0] = "x"
-            print(seatA)
 
-        elif s == "B":
-            seatB[0] = "x"
-            print(seatA)
-        
-        elif s == "C":
-            seatC[0] = "x"
-            print(seatA)
-
-        elif s == "D":
-            seatD[0] = "x"
-
-        for i in range(0,7):
-            print(i + 1, seatA[i],seatB[i],seatC[i],seatD[i])
-        
-    if r == 1:    
-        if s == "A":
-            seatA[1] = "x"
-            print(seatA)
-
-        elif s == "B":
-            seatB[1] = "x"
-            print(seatA)
-        
-        elif s == "C":
-            seatC[1] = "x"
-            print(seatA)
-
-        elif s == "D":
-            seatD[1] = "x"
-
-        for i in range(0,7):
-            print(i + 1, seatA[i],seatB[i],seatC[i],seatD[i])
     
-    if r == 2:    
+    clear()
+
+    if r >= 0 or r <= 7 :    
         if s == "A":
-            seatA[2] = "x"
-            print(seatA)
+            seatA[r] = "x"
 
         elif s == "B":
-            seatB[2] = "x"
-            print(seatA)
+            seatB[r] = "x"
+            
         
         elif s == "C":
-            seatC[2] = "x"
-            print(seatA)
+            seatC[r] = "x"
+            
 
         elif s == "D":
-            seatD[2] = "x"
+            seatD[r] = "x"
 
+        print("row")
         for i in range(0,7):
             print(i + 1, seatA[i],seatB[i],seatC[i],seatD[i])
 
-    if r == 3:    
-        if s == "A":
-            seatA[3] = "x"
-            print(seatA)
+    answer = input("Would you like to add another seat?: ")
 
-        elif s == "B":
-            seatB[3] = "x"
-            print(seatA)
-        
-        elif s == "C":
-            seatC[3] = "x"
-            print(seatA)
+    clear()
 
-        elif s == "D":
-            seatD[3] = "x"
+    #for i in range(0,7):
+        #if r >= 0 or r <= 7 and s == "A": 
+        #if seatA[i] == "x":
+            #print("Eror")
+           # answer = input("Would you like to add another seat?: ")
 
-        for i in range(0,7):
-            print(i + 1, seatA[i],seatB[i],seatC[i],seatD[i])
 
-    if r == 4:    
-        if s == "A":
-            seatA[4] = "x"
-            print(seatA)
-
-        elif s == "B":
-            seatB[4] = "x"
-            print(seatA)
-        
-        elif s == "C":
-            seatC[4] = "x"
-            print(seatA)
-
-        elif s == "D":
-            seatD[4] = "x"
-
-        for i in range(0,7):
-            print(i + 1, seatA[i],seatB[i],seatC[i],seatD[i])
-
-    if r == 5:    
-        if s == "A":
-            seatA[5] = "x"
-            print(seatA)
-
-        elif s == "B":
-            seatB[5] = "x"
-            print(seatA)
-        
-        elif s == "C":
-            seatC[5] = "x"
-            print(seatA)
-
-        elif s == "D":
-            seatD[5] = "x"
-
-        for i in range(0,7):
-            print(i + 1, seatA[i],seatB[i],seatC[i],seatD[i])
+print("Have a good day =D")
     
 
 
