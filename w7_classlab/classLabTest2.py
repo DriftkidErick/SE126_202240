093cd0a083c53e5aae32986ba1ef2c4f0dfa2466
DriftkidErick <94418693+DriftkidErick@users.noreply.github.com>
2022-09-06 13:24:18 -0400

    Done

--- w7_classlab/classLabTest2.py
+++ w7_classlab/classLabTest2.py
@@ -1,47 +1,28 @@
-from os import system, name
-from time import sleep
-
-def clear():
-   # for windows
-   if name == 'nt':
-      _ = system('cls')
-   # for mac 
-   else:
-    _ = system('clear')
-
-#Functions:
-def rowPick():
-    #asks the user for the row and seat and returns it
-    r = int(input("\n\tPlease enter the row of the seat you want to pick: "))
-
-    while r < 1 and r > 7:
-        r = int(input("\n\t**INVALID INPUT** \nPlease enter the row of the seat you want to pick: "))
-
-
-    return [(r - 1)]
-
 def seatPick():
+    #asks the user for the row and seat and returns it
+    r = int(input("\n\tPlease select a seat Row [1 - 7]: "))
+    while r < 1 or r > 7:
+        r = int(input("\n\t**INVALID INPUT** \nPlease select a seat Row [1 - 7]: "))
 
-    s = input("\tPlease enter the seat letter you would like to pick: ").upper()
+    s = str(input("\tPlease select the seat you would like [A - D]: ")).upper()
+    while s != "A" and s != "B" and s != "C" and s != "D":
+        s = str(input("\t**INVALID INPUT** \nPlease select the seat you would like [A - D]: ")).upper()
 
-    while s != 'A' and s != 'B' and s != 'C' and s != 'D':
-        s = input("\t**INVALID INPUT** \nPlease enter the seat letter you would like to pick: ").upper()
-    
-    return [s]
+    return [(r - 1), s]
+
 
 def repeat():
     #asks the user if they want to enter another seat
-    a = input("\nWould you like to enter another seat? [y/n]: ")
+    a = input("\nWould you like to enter another seat? [y/n]: ").lower()
 
     if a != "y" and a != "n":
         print("\n\t\t**INVALID INPUT** PLEASE TRY AGAIN.")
-        a = input("\nWould you like to enter another seat that you would like? [y/n]: ")
-    
+        a = input("\nWould you like to enter another seat that you would like? [y/n]: ").lower()
+
     return a
 
-#MAIN CODE------------------------------------------------------------------------------------------------------------------------
-
-answer = "y"
+#---------------MAIN CODE---------------
+import os
 
 #lists
 seatA = ['A','A','A','A','A','A','A']
@@ -54,37 +35,49 @@ for i in range(0, (len(seatA))):
     #displays original seating chart
     print("\n{}\t\t{}\t{}\t{}\t{}\t".format((i + 1), seatA[i], seatB[i], seatC[i], seatD[i]))
 
+answer = "y"
 while answer == "y":
-
     seatTaken = "False"
-    row = rowPick()
     seat = seatPick()
+    os.system('cls')
 
-    if seatA[row[0]] == 'X':
-            if row[1] == 1:
-                if seat[1] == 'A':
-                    seatTaken = "True"
+    if seatA[seat[0]] == 'X':
+            if seat[1] == 'A':
+                seatTaken = "True"
+    if seatB[seat[0]] == 'X':
+            if seat[1] == 'B':
+                seatTaken = "True"
+    if seatC[seat[0]] == 'X':
+            if seat[1] == 'C':
+                seatTaken = "True"
+    if seatD[seat[0]] == 'X':
+            if seat[1] == 'D':
+                seatTaken = "True"
 
     for i in range(0, (len(seatA))):
-            
+
         if seatTaken == "False":
             #checks the input from user against this if/else tree
-            if seatA[row[0]] != 'X':
-                if row[0] == 1:
-
-                 if seat[1] == 'A':
-                        seatA[seat[0]] = 'X'
-
-        
+            if seatA[seat[0]] != 'X':
+                if seat[1] == 'A':
+                    seatA[seat[0]] = 'X'
+            if seatB[seat[0]] != 'X':
+                if seat[1] == 'B':
+                    seatB[seat[0]] = 'X'
+            if seatC[seat[0]] != 'X':
+                if seat[1] == 'C':
+                    seatC[seat[0]] = 'X'
+            if seatD[seat[0]] != 'X':
+                if seat[1] == 'D':
+                    seatD[seat[0]] = 'X'
 
             #prints updated seating chart
             print("\n{}\t\t{}\t{}\t{}\t{}\t".format((i + 1), seatA[i], seatB[i], seatC[i], seatD[i]))
-        
+
     if seatTaken == "True":
+        print("\n{}\t\t{}\t{}\t{}\t{}\t".format((i + 1), seatA[i], seatB[i], seatC[i], seatD[i]))
         print("\nThis seat is already taken. Please choose another.")
-    
-    
-    
+
     answer = repeat()
 
-print("Thank you for using our program =D")
+print("\n\n\t\t\tGoodbyeeeeee!")
--- w7_classlab/classtest.py
+++ w7_classlab/classtest.py
@@ -1,4 +1,16 @@
+#Class lab W7 Python Pals
+
+#prompt: Write a Python program to assign passengers seats in an airplane. 
+
+#variable definitions:
+#row is the row in which the user would like to sit in
+#s is the seat they would like to sit in
+
+
+
+#function definitions below:
 from os import system, name
+
 from time import sleep
 
 def clear():
@@ -9,21 +21,29 @@ def clear():
    else:
     _ = system('clear')
 
+def seatPick():
+    #asks the user for the row and seat and returns it
+    row = int(input("\n\tPlease select a seat Row [1 - 7]: "))
+
+    while row < 1 or row > 7:
+        row = int(input("\n\t**INVALID INPUT** \nPlease select a seat Row [1 - 7]: "))
+
+    s = str(input("\tPlease select the seat you would like [A - D]: ")).upper()
+
+    while s != "A" and s != "B" and s != "C" and s != "D":
+        s = str(input("\t**INVALID INPUT** \nPlease select the seat you would like [A - D]: ")).upper()
+
+    return [(row - 1), s]
+
 def more():
+    #asks the user if they want to enter another seat
+    a = input("\nWould you like to enter another seat? [y/n]: ").lower()
 
-    answer = input("Would you like to add another seat? [y/n]: ").lower()
+    return a
 
-    if answer == "n".lower():
-        print("have a good day")
+#Main code below----------------------------------------------------------
 
-    elif answer != "y" and answer != "n":
-        answer = input("Would you like to add another seat? [y/n]: ").lower()
-
-    
-    return (answer)
-
-#Main Code Below--------------------------
-
+#available seat list
 seatA = ['A','A','A','A','A','A','A']
 seatB = ['B','B','B','B','B','B','B']
 seatC = ['C','C','C','C','C','C','C']
@@ -31,38 +51,68 @@ seatD = ['D','D','D','D','D','D','D']
 
 print("Row")
 
-for i in range(0,7):
-    print("{}  {} {}\t{} {}".format(i + 1, seatA[i],seatB[i],seatC[i],seatD[i]))
+for i in range(0 , 7): #displays original data
+    print("\n{}\t\t{}\t{}\t{}\t{}\t".format((i + 1), seatA[i], seatB[i], seatC[i], seatD[i]))
 
 answer = "y"
 
-while answer =="y":
-    answer = int(input("Please enter the row you would like to sit in [1-7]: "))
+while answer == "y":
 
-    r = (answer -1)    
+    flag = "False"
 
-    s = input("Please enter the seat you would like [A - D]: ").upper()
+    seat = seatPick()
 
-    if r >= 0 or r <= 7 :    
-        if s == "A":
-            seatA[r] = "x"
+    clear()
+
+    if seatA[seat[0]] == "x":
+            if seat[1] == "A":
+                flag = "True"
+    
+    if seatB[seat[0]] == "x":
+            if seat[1] == "B":
+                flag = "True"
+    
+    if seatC[seat[0]] == "x":
+            if seat[1] == "C":
+                flag = "True"
+    
+    if seatD[seat[0]] == "x":
+            if seat[1] == "D":
+                flag = "True"
+
+    for i in range(0,7):
+
+        if flag == "False": 
+
+            if seatA[seat[0]] != "x":
+                if seat[1] == "A":
+                    seatA[seat[0]] = "x"
             
-
-        elif s == "B":
-            seatB[r] = "x"
+            if seatB[seat[0]] != "x":
+                if seat[1] == "B":
+                    seatB[seat[0]] = "x"
             
-        
-        elif s == "C":
-            seatC[r] = "x"
-            
+            if seatC[seat[0]] != "x":
+                if seat[1] == "C":
+                    seatC[seat[0]] = "x"
 
-        elif s == "D":
-            seatD[r] = "x"
+            if seatD[seat[0]] != "x":
+                if seat[1] == "D":
+                    seatD[seat[0]] = "x"
 
-        for i in range(0,7):
-            print(i + 1, seatA[i],seatB[i],seatC[i],seatD[i])
+            #prints updated file
+            print("\n{}\t\t{}\t{}\t{}\t{}\t".format((i + 1), seatA[i], seatB[i], seatC[i], seatD[i]))
 
-    more() #The function that asks the user if they want to add another seat
+    if flag == "True": # if the seat it take in the file print seat is not available
+        print("\n{}\t\t{}\t{}\t{}\t{}\t".format((i + 1), seatA[i], seatB[i], seatC[i], seatD[i]))
+        print("\nThis seat is already taken. Please choose another.")
+
+    answer = more()
+
+    while answer != "y" and answer != "n": #checks for user error
+        answer = input("\nWould you like to enter another seat? [y/n]: ").lower()
 
 
-print("out of loop")
+
+print("Thank you for using ")
+
